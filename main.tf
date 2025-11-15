96b5dc22f7c7ff0500b287914190bd92b306c847
vah554000 <vah554000@gmail.com>
2025-11-15 22:47:54 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,8 +34,8 @@ provider "aci" {
 #}
 
 
-resource "aci_vlan_pool" "vlan_pool_vahid_11" {
-  name        = "vlan_pool_vahid_11"
+resource "aci_vlan_pool" "vlan_pool_vahid_12" {
+  name        = "vlan_pool_vahid_12"
   alloc_mode  = "dynamic"   # static | dynamic
   description = "Production VLAN Pool created via Terraform"
 }
